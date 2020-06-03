fbf2082853440ef0d8ccc7d7e9cba972b8f8094b
MarcosRuginski <marcosrobertoruginski@gmail.com>
2020-06-03 19:11:53 -0300

    form create-point

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,4 +1,3 @@
-
 function populateUFs() {
   const ufSlect = document.querySelector("select[name=uf]");
   fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
@@ -24,13 +23,15 @@ function getCities(event) {
   stateInput.value = event.target.options[indexOfSelectState].text;
 
   const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+  
+  citySelect.innerHTML = "<option value>Selecione a Cidade</option>";
+  citySelect.disabled = true;
 
   fetch(url)
   .then(res => res.json())
   .then(cities => {
-
     for( city of cities ) {
-      citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>` 
+      citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>` 
     }
 
     citySelect.disabled = false;
@@ -42,3 +43,43 @@ function getCities(event) {
  document
   .querySelector("select[name=uf]")
   .addEventListener("change", getCities)
+
+
+  // Itens de Coleta
+  const itemsToCollect = document.querySelectorAll(".items-grid li");
+
+  for (const item of itemsToCollect) {
+    item.addEventListener("click", handleSelectedItem)
+  }
+
+  const collectedItems = document.querySelector("input[name=items]");
+
+  let selectedItems = [];
+
+  function handleSelectedItem(event) {
+    const itemLi = event.target;
+
+    itemLi.classList.toggle("selected");
+
+    const itemId = itemLi.dataset.id;
+
+    const alreadySelected = selectedItems.findIndex(item => item == itemId);
+
+    // Se ja estiver selecionado
+    if(alreadySelected >= 0) {
+      // Tira a seleção
+      const filteredItems = selectedItems.filter(item => {
+        const itemIsDifferent = item != itemId;
+        return itemIsDifferent;
+      });
+
+      selectedItems = filteredItems;
+    } else {
+       // se não estiver selecionado
+       // adiciona a seleção
+       selectedItems.push(itemId);
+    }
+
+    collectedItems.value = selectedItems;
+
+  }
